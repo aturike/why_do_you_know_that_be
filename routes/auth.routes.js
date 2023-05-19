7d7029ca2d05cc3a3dfe26ef11d96a63d1562303
Gus <august.colonna@gmail.com>
2023-05-19 13:12:01 +0200

    token added to backend

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { isAuthenticated } = require("../middleware/jwt.middelware");
 
 router.post("/signup", async (req, res, next) => {
   const salt = bcryptjs.genSaltSync(13);
@@ -20,15 +22,28 @@ router.post("/signup", async (req, res, next) => {
 
 router.post("/login", async (req, res, next) => {
   //checking if the user is in the DB
-  const userExists = User.findOne({ email: req.body.email })
+  const userExists = User.findOne({ email: req.body.email });
 
-  if(userExists){
-    if(bcryptjs.compareSync(req.body.password, userExists.password)){
-      res.status(201).json("Sign in good");
-    }else{
-      res.json("incorrect password")
+  if (userExists) {
+    if (bcryptjs.compareSync(req.body.password, userExists.password)) {
+      const authToken = jwt.sign(
+        { userId: userExists._id },
+        process.env.TOKEN_SECRET,
+        {
+          algorithm: "HS256",
+          expiresIn: "24h",
+        }
+      );
+      res.json(authToken);
+    } else {
+      res.json("incorrect password or username");
     }
   }
+});
+
+router.get("/verify", isAuthenticated, async (req, res) => {
+  const user = await User.findById(req.payload.userId);
+  res.json({ message: "user is autenticated", payload: user });
 });
 
 module.exports = router;
